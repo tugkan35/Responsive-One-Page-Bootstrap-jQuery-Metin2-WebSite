e901ec8212eabf3cdc11f0c3fae1dfc2a5e18d00
Yaşar Tuğkan Zenger <58388310+tugkan35@users.noreply.github.com>
2026-09-24 11:27:49 +0300

    Add files via upload

---
+++ js/scrollTop.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+        var offset = 500;
+        var duration = 500;
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.crunchify-top').fadeIn(duration);
+            } else {
+                jQuery('.crunchify-top').fadeOut(duration);
+            }
+        });
+        jQuery('.crunchify-top').click(function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
+    });
